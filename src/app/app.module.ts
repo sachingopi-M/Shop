90ea891203f2418c9212c24bd0d7cf702e1ab8c8
sachingopi-M <129177787+sachingopi-M@users.noreply.github.com>
2023-03-28 18:12:37 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+
+//   newpost ="";
+//   enteredvalue ="";
+
+// onaddpost(){
+// this.newpost=this.enteredvalue;
+
+// }
+
+// booleandata=false;
+// color='red';
+
+userarray=['name','salary','date'];
+userlist=[{name:'ragul',salary:'10000',location:'pondy',skillset:['angular','java','mongodb']},
+{name:'ragul',salary:'10000',location:'pondy',skillset:['angular','mongodb']},
+{name:'ragul',salary:'10000',location:'pondy',skillset:['angular','java',]}]
+}
